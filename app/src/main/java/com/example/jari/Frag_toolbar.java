6eada438eb7a42a0dbbc2d835f2eb05dc681f9ed
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2020-05-16 17:34:59 +0900

    Toolbar ~ing

---
+++ app/src/main/java/com/example/jari/Frag_toolbar.java
@@ -0,0 +1,31 @@
+package com.example.jari;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag_toolbar extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,
+                             @NonNull Bundle saveInstanceState) {
+        view = (View) inflater.inflate(R.layout.activity_main, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main_toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
