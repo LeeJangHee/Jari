2808febe26cf2ccf51a69dd2bb3c091c2ab85d2e
janghee <sj90947@gmail.com>
2020-05-28 18:38:47 +0900

    HomeView BestMenu View

--- app/src/main/java/com/example/jari/MainActivity.java
+++ app/src/main/java/com/example/jari/MainActivity.java
@@ -3,15 +3,13 @@ package com.example.jari;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
