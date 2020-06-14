43bc847be68059cdd53f6ea33bd28fc0f2195a69
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2020-06-14 16:49:39 +0900

    ListView >> RecyclerView change2

--- app/src/main/java/com/example/jari/Frag_home.java
+++ app/src/main/java/com/example/jari/Frag_home.java
@@ -12,84 +12,43 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
-public class Frag_home extends Fragment {
-    View view;
-    TextView toolbar_title;
-    String str_name;
+public class Frag_home extends Fragment implements View.OnClickListener {
+    private View view;
+    private TextView toolbar_title;
+    private String str_name;
+
+    private ImageView ig_bestMenu;
+    private ImageView ig_menuKor;
+    private ImageView ig_menuJp;
+    private ImageView ig_menuCh;
+    private ImageView ig_menuWf;
+    private ImageView ig_menuCafe;
+    private ImageView ig_menuBeer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,
                              @NonNull Bundle saveInstanceState) {
         view = (View) inflater.inflate(R.layout.frag_home, container, false);
-        view.setOverScrollMode(View.OVER_SCROLL_IF_CONTENT_SCROLLS);
 
         toolbar_title = (TextView) ((MainActivity) getActivity()).findViewById(R.id.toolbar_title);
 
-        ImageView ig_bestMenu = (ImageView) view.findViewById(R.id.menuBest);
-        ImageView ig_menuKor = (ImageView) view.findViewById(R.id.menuKor);
-        ImageView ig_menuJp = (ImageView) view.findViewById(R.id.menuJp);
-        ImageView ig_menuCh = (ImageView) view.findViewById(R.id.menuCh);
-        ImageView ig_menuWf = (ImageView) view.findViewById(R.id.menuWf);
-        ImageView ig_menuCafe = (ImageView) view.findViewById(R.id.menuCafe);
-        ImageView ig_menuBeer = (ImageView) view.findViewById(R.id.menuBeer);
+        ig_bestMenu = (ImageView) view.findViewById(R.id.menuBest);
+        ig_menuKor = (ImageView) view.findViewById(R.id.menuKor);
+        ig_menuJp = (ImageView) view.findViewById(R.id.menuJp);
+        ig_menuCh = (ImageView) view.findViewById(R.id.menuCh);
+        ig_menuWf = (ImageView) view.findViewById(R.id.menuWf);
+        ig_menuCafe = (ImageView) view.findViewById(R.id.menuCafe);
+        ig_menuBeer = (ImageView) view.findViewById(R.id.menuBeer);
 
 
-        ig_bestMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_best);
-                menuOnClick(str_name, new Frag_home_menu_best());
-            }
-        });
-
-        ig_menuKor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_kor);
-                menuOnClick(str_name, new Frag_home_menu_kor());
-            }
-        });
-
-        ig_menuJp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_jp);
-                menuOnClick(str_name, new Frag_home_menu_jp());
-            }
-        });
-
-        ig_menuCh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_ch);
-                menuOnClick(str_name, new Frag_home_menu_ch());
-            }
-        });
-
-        ig_menuWf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_wf);
-                menuOnClick(str_name, new Frag_home_menu_wf());
-            }
-        });
-
-        ig_menuCafe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_cafe);
-                menuOnClick(str_name, new Frag_home_menu_cafe());
-            }
-        });
-
-        ig_menuBeer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str_name = getString(R.string.menu_beer);
-                menuOnClick(str_name, new Frag_home_menu_beer());
-            }
-        });
+        ig_bestMenu.setOnClickListener(this);
+        ig_menuKor.setOnClickListener(this);
+        ig_menuJp.setOnClickListener(this);
+        ig_menuCh.setOnClickListener(this);
+        ig_menuWf.setOnClickListener(this);
+        ig_menuCafe.setOnClickListener(this);
+        ig_menuBeer.setOnClickListener(this);
 
         return view;
     }
@@ -104,4 +63,38 @@ public class Frag_home extends Fragment {
         actionBar_bestMenu.setHomeAsUpIndicator(R.drawable.ic_arrow_back_20px);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.menuBest:
+                str_name = getString(R.string.menu_best);
+                menuOnClick(str_name, new Frag_home_menu_best());
+                break;
+            case R.id.menuKor:
+                str_name = getString(R.string.menu_kor);
+                menuOnClick(str_name, new Frag_home_menu_kor());
+                break;
+            case R.id.menuCh:
+                str_name = getString(R.string.menu_ch);
+                menuOnClick(str_name, new Frag_home_menu_ch());
+                break;
+            case R.id.menuJp:
+                str_name = getString(R.string.menu_jp);
+                menuOnClick(str_name, new Frag_home_menu_jp());
+                break;
+            case R.id.menuWf:
+                str_name = getString(R.string.menu_wf);
+                menuOnClick(str_name, new Frag_home_menu_wf());
+                break;
+            case R.id.menuCafe:
+                str_name = getString(R.string.menu_cafe);
+                menuOnClick(str_name, new Frag_home_menu_cafe());
+                break;
+            case R.id.menuBeer:
+                str_name = getString(R.string.menu_beer);
+                menuOnClick(str_name, new Frag_home_menu_beer());
+                break;
+
+        }
+    }
 }
